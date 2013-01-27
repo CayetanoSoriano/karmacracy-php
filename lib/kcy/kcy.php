e23ad2af49cce1323e324e8e0d1788133c0eace5
Cayetano Soriano <neoshadybeat@gmail.com>
2013-01-27 16:16:47 +0100

    Added new methods

--- lib/kcy/kcy.php
+++ lib/kcy/kcy.php
@@ -4,19 +4,30 @@ use Buzz\Browser;
 
 class kcy {
 
-	protected $user, $keypass, $appkey, $browser;
+	protected $user, $keypass, $appkey;
+	protected $browser;
+	protected $base_url;
+	protected $json_output;
 
 	function __construct($user = NULL, $keypass = NULL, $appkey = NULL){
         $this->browser = new Browser();
 		$this->user = $user;
 		$this->keypass = $keypass;
 		$this->appkey = $appkey;
+		$this->base_url = 'http://karmacracy.com/api/v1/';
+		$this->json_output = TRUE;
 	}
 
+	public function setBaseUrl($base_url)
+	{
+		$this->base_url = $base_url;
+		return $this;
+	}
 
 	public function setUser($user)
 	{
 		$this->user = $user;
+		return $this;
 	}
 
 	public function getUser()
@@ -27,6 +38,7 @@ class kcy {
 	public function setKeypass($keypass)
 	{
 		$this->user = $keypass;
+		return $this;
 	}
 
 	public function getKeypass()
@@ -37,6 +49,12 @@ class kcy {
 	public function setAppkey($appkey)
 	{
 		$this->user = $appkey;
+		return $this;
+	}
+
+	public function setJsonOutput($value){
+		$this->json_output = ($value === TRUE) ? TRUE : FALSE;
+		return $this;
 	}
 
 	public function getAppkey()
@@ -45,14 +63,29 @@ class kcy {
 	}
 
 
-	public function shortUrl($url, $json = TRUE)
+	public function getShortUrl($url)
 	{
-		$url = "http://kcy.me/api/?u=$this->user&key=$this->keypass&url=$url";
+		$base_url = 'http://kcy.me/api/';
+		$url = "$base_url?u=$this->user&key=$this->keypass&url=$url";
 		$url .= $json ? "&format=json" : '';
 		print $url;
 		$response = $this->browser->get($url)->getContent();
 
-		return ($json ? json_decode($response) : $response);
+		return $this->getResponse($response);
+	}
+
+
+	public function getKcyData($kcy){
+		$endpoint = 'kcy/';
+		$parameters = "$kcy?appkey=$this->appkey";
+		$url = $this->base_url.$endpoint.$parameters;
+		$response = $this->browser->get($url)->getContent();
+
+		return $this->getResponse($response);
+	}
+
+	protected function getResponse($data){
+		return (($this->json_output === TRUE) ? $data : json_decode($data));
 	}
 
 }
