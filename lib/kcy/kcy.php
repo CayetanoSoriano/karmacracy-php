38af4c3fd83462f6a413c23797db1c2a7ab583df
Cayetano Soriano <neoshadybeat@gmail.com>
2013-08-31 18:02:40 +0200

    Changed buzz by guzzle

--- lib/kcy/kcy.php
+++ lib/kcy/kcy.php
@@ -1,79 +1,27 @@
 <?php
+
 namespace kcy;
-use Buzz\Browser;
+use Guzzle\Http\Client;
 
 class kcy {
 
-	protected $keypass, $appkey;
+	protected static $shortUrlBase = "http://kcy.me/api/";
+	protected static $baseUrl = 'http://karmacracy.com/api/';
+	protected static $apiVersion = 'v1/';
+	protected $username, $keypass, $appkey;
 	protected $browser;
-	protected $base_url;
-	protected $json_output;
 
-	function __construct($keypass, $appkey){
-        $this->browser = new Browser();
+	function __construct($username, $appkey, $keypass) {
+		$this->browser = new Client();
+		$this->username = $username;
 		$this->keypass = $keypass;
 		$this->appkey = $appkey;
-		$this->base_url = 'http://karmacracy.com/api/v1/';
-		$this->json_output = TRUE;
 	}
 
-	public function setBaseUrl($base_url)
-	{
-		$this->base_url = $base_url;
-		return $this;
+	public function shortUrl($url) {
+		$request = $this->browser->get(self::$shortUrlBase, array(), array(
+			'query' => array('u' => $this->username, 'key' => $this->appkey, 'url' => $url)
+			));
+		return (string) $this->browser->send($request);
 	}
-
-	public function setKeypass($keypass)
-	{
-		$this->user = $keypass;
-		return $this;
-	}
-
-	public function getKeypass()
-	{
-		return $this->keypass;
-	}
-
-	public function setAppkey($appkey)
-	{
-		$this->user = $appkey;
-		return $this;
-	}
-
-	public function setJsonOutput($value){
-		$this->json_output = ($value === TRUE) ? TRUE : FALSE;
-		return $this;
-	}
-
-	public function getAppkey()
-	{
-		return $this->appkey;
-	}
-
-
-	public function getShortUrl($url)
-	{
-		$base_url = 'http://kcy.me/api/';
-		$url = "$base_url?u=$this->user&key=$this->keypass&url=$url";
-		$url .= $json ? "&format=json" : '';
-		print $url;
-		$response = $this->browser->get($url)->getContent();
-
-		return $this->getResponse($response);
-	}
-
-
-	public function getKcyData($kcy){
-		$endpoint = 'kcy/';
-		$parameters = "$kcy?appkey=$this->appkey";
-		$url = $this->base_url.$endpoint.$parameters;
-		$response = $this->browser->get($url)->getContent();
-
-		return $this->getResponse($response);
-	}
-
-	protected function getResponse($data){
-		return (($this->json_output === TRUE) ? $data : json_decode($data));
-	}
-
 }
